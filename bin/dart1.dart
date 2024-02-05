8b809e08799e343471ddbd068b4a90d05933b15e
Ana Gilabert Lucas <anaymenta@gmail.com>
2024-02-05 17:52:56 +0100

    Formato Hola Mundo y fecha en español

--- bin/dart1.dart
+++ bin/dart1.dart
@@ -4,8 +4,8 @@ void main(List<String> arguments) {
     // Obtener la fecha y hora actual
     DateTime now = DateTime.now();
     // Formatear la fecha y hora
-    String formattedDateTime = "${now.year}-${now.month}-${now.day} ${now.hour}:${now.minute}:${now.second}";
+    String formattedDateTime = "${now.day}-${now.month}-${now.year} ${now.hour}:${now.minute}:${now.second}";
     // Imprimir el mensaje con la fecha y hora
     var dart_application_1;
-    print('Hello world: ${dart_application_1.calculate()}! ($formattedDateTime)');
+    print('Hola Mundo ${dart_application_1.calculate()}! ($formattedDateTime)');
 }
